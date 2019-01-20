5bee5aea2f0e3ffc4570eb1766916de5dbf72472
Emp-Norton <jamesjelenko@gmail.com>
2019-01-20 09:23:32 -0800

    add parity determination function + minor correction in reduce call

--- 10.js
+++ 10.js
@@ -5,6 +5,9 @@
   	}
   	return true
   }
-  const result = new Array(2000000).fill(3).map((n, i) => i).reduce((s, n) => { return prime(n) && !even(n) ? s += n : s},0);
+  const even = (n) => {
+  	return n % 2 === 0
+  }
+  const result = new Array(2000000).fill(3).map((n, i) => i).reduce((s, n) => { return prime(n) && !even(n) ? s += n : s},1);
   console.log(result);
 })(process.argv.slice(2)[0])
