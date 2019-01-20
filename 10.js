a48490bff8d4c048baec8ebcb572c73b5abe1e12
Emp-Norton <jamesjelenko@gmail.com>
2019-01-20 09:21:52 -0800

    add prime determination function

--- 10.js
+++ 10.js
@@ -1,5 +1,10 @@
-function(n){
-  // this is not quite correct, but it's late and I'm tired. Fix tomorrow.
+(function(n){
+  const prime = (n) => {
+  	for (let i = 2; i <= Math.sqrt(n); i++) {
+  		if (n % i === 0) return false
+  	}
+  	return true
+  }
   const result = new Array(2000000).fill(3).map((n, i) => i).reduce((s, n) => { return prime(n) && !even(n) ? s += n : s},0);
   console.log(result);
 })(process.argv.slice(2)[0])
