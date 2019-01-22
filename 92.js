5453a4ea52270f093920a08c0b7af21295e4fdfb
Emp-Norton <jamesjelenko@gmail.com>
2019-01-22 14:43:03 -0800

    add problem statement at top, note about memoization

--- 92.js
+++ 92.js
@@ -1,36 +1,3 @@
-'use strict';
-
-const squareDigits = (n) => {
-  return n.toString()
-	  .split('')
-	  .map(i => parseInt(i) * parseInt(i))
-	  .reduce((sum, num) => { return sum += num }, 0);
-}
-
-const chain = (n, orig) => {
-  while (n != 89) {
-    if (n == 1) return false
-    // console.log(`N: ${n}, Orig: ${orig}`)
-    n = squareDigits(n);
-  }
-  if (n == 89) {
-    // console.log(`Adding ${orig}`)
-    return true
-  }
-  return false
-}
-
-const main = (n) => {
-  let targetNums = {};
-  for (let i = 2; i <= n; i++) {
-    if (chain(i, i)) {
-      targetNums[i] = true;
-    }
-  }
-  console.log(Object.keys(targetNums).length);
-}
-
-main(10000000);
 /*
 A number chain is created by continuously adding the square of the digits in a number to form a new number until it has been seen before.
 
@@ -43,3 +10,33 @@ Therefore any chain that arrives at 1 or 89 will become stuck in an endless loop
 
 How many starting numbers below ten million will arrive at 89?
 */
+'use strict';
+
+const squareDigits = (n) => {
+  return n.toString()
+	  .split('')
+	  .map(i => parseInt(i) * parseInt(i))
+	  .reduce((sum, num) => { return sum += num }, 0);
+}
+
+const chain = (n, orig) => {
+  while (n != 89) {
+    if (n == 1) return false
+    n = squareDigits(n);
+  }
+  if (n == 89) return true
+  return false
+}
+
+const main = (n) => {
+  let visited = {} // memoize to avoid repeats
+  let targetNums = {};
+  for (let i = 2; i <= n; i++) {
+    if (chain(i, i)) {
+      targetNums[i] = true;
+    }
+  }
+  console.log(Object.keys(targetNums).length);
+}
+
+main(10000000);
