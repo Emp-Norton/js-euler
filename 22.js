e7405ed078dfbcfbb99a09bfb9d60262443075af
Emp-Norton <jamesjelenko@gmail.com>
2019-01-22 13:54:31 -0800

    cleanup II

--- 22.js
+++ 22.js
@@ -1,12 +1,11 @@
 const fs = require('fs');
 
 const processName = (name) => {
-  name = name.replace(/\"/g, ""); // nix quotes
-  let result = name.split('').map(char => char
+  return name.replace(/\"/g, "") // nix quotes
+    .split('').map(char => char
     .toLowerCase()
     .charCodeAt(0) - 96)
     .reduce((sum, num) => { return sum += num }, 0);
-  return result
 }
 
 const processData = (data) => {
