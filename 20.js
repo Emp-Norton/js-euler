3ae3e933cf20d680d98c8594fa33952118187df6
Emp-Norton <jamesjelenko@gmail.com>
2019-01-22 14:03:28 -0800

    add split and sum function -- needs revision to fix sci-method representation

--- 20.js
+++ 20.js
@@ -1,11 +1,22 @@
 "use strict"
 const args = process.argv.slice(2)[0];
-console.log(args);
 
 const factorial = (n) => {
 	// simple impl before memoize
   const res = n <= 2 ? n : n * factorial(n - 1);
+  console.log(res);
   return res
 }
 
-factorial(parseInt(args));
+const splitAndSum = (n) => {
+  console.log(Number(n));
+  const res = n.toString()
+    .split('')
+    .map(i => parseInt(i))
+    .reduce((sum, num) => { return sum += num }, 0);
+    console.log(res);
+    return res
+}
+
+let f = factorial(parseInt(args));
+//splitAndSum(f);
